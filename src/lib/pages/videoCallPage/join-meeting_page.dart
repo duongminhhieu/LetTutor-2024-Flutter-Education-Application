e521a9bddae3037a9031c907af81f34dc1013143
20120473 <76527212+duongminhhieu@users.noreply.github.com>
2023-12-23 01:09:58 +0700

    fix end time counttimer

--- src/lib/pages/videoCallPage/join-meeting_page.dart
+++ src/lib/pages/videoCallPage/join-meeting_page.dart
@@ -26,7 +26,7 @@ class _JoinMeetingPageState extends State<JoinMeetingPage> {
     super.initState();
     // endTime = DateTime.now().add(const Duration(seconds: 10));
     endTime = DateTime.fromMillisecondsSinceEpoch(
-        upcomingClass.scheduleDetailInfo!.endPeriodTimestamp!);
+        upcomingClass.scheduleDetailInfo!.startPeriodTimestamp!);
   }
 
   void onEnd() {
